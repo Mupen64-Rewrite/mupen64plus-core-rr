9b210dfd21c69305630633864211f5e922575552
Madghostek <madghostek@gmail.com>
2021-09-17 23:19:02 +0200

    more vcr, now plays m64 correctly

--- src/VCR/VCR.cpp
+++ src/VCR/VCR.cpp
@@ -17,9 +17,10 @@ extern "C" {
 }
 
 static std::vector<BUTTONS> *gMovieBuffer = NULL; //holds reference to m64 data if movie is being played, otherwise NULL
+static SMovieHeader* gMovieHeader = NULL;
 static VCRState VCR_state = VCRState::VCR_IDLE;
 static bool VCR_readonly;
-static unsigned curFrame = 0;
+static unsigned curSample = 0; //doesnt account in for multiple controllers, used as a pointer for vector
 
 #define BUFFER_GROWTH 256
 
@@ -27,28 +28,38 @@ void VCR_StopMovie()
 {
 	VCR_state = VCR_IDLE;
 	delete gMovieBuffer;
+	delete gMovieHeader;
+	gMovieBuffer = NULL;
+	gMovieHeader  = NULL;
 }
 
 
-void VCR_SetKeys(BUTTONS keys)
+void VCR_SetKeys(BUTTONS keys, unsigned channel)
 {
-	(*gMovieBuffer)[curFrame++].Value = keys.Value;
-	if (curFrame >= gMovieBuffer->size())
+	(*gMovieBuffer)[curSample++].Value = keys.Value;
+	if (curSample >= gMovieBuffer->size())
 	{
 		gMovieBuffer->resize(gMovieBuffer->size() + BUFFER_GROWTH); //impending doom approaches...
 	}
 }
 
-void VCR_GetKeys(BUTTONS* keys)
+BOOL VCR_GetKeys(BUTTONS* keys, unsigned channel)
 {
-	//keys->Value = (*gMovieBuffer)[curFrame++].Value; //get the value, then advance frame
-	//the frame we just provided could be last, check for bounds.
-	//frames are 0-indexed, size is 1-indexed, therefore >=
-	if (curFrame >= gMovieBuffer->size())
+	//gives out inputs for channels that are present in m64 header
+	//less bloated than using the flags itself
+	if (gMovieHeader->cFlags.present & (1 << channel))
 	{
-		//@TODO somehow notify frontend about that
-		VCR_StopMovie();
+		keys->Value = (*gMovieBuffer)[curSample++].Value; //get the value, then advance frame
+		//the frame we just provided could be last, check for bounds.
+		//frames are 0-indexed, size is 1-indexed, therefore >=
+		if (curSample >= gMovieBuffer->size())
+		{
+			//@TODO somehow notify frontend about that
+			VCR_StopMovie();
+			return true; //ended
+		}
 	}
+	return false;
 }
 
 BOOL VCR_IsPlaying()
@@ -66,6 +77,12 @@ BOOL VCR_SetReadOnly(BOOL state)
 	return VCR_readonly = state;
 }
 
+unsigned VCR_GetCurFrame()
+{
+	if (!VCR_IsPlaying()) return -1;
+	return curSample / gMovieHeader->num_controllers;
+}
+
 m64p_error VCR_StartMovie(char* path)
 {
 	//@TODO close previous m64 if loading while vcr state is not idle (ask warning in frontend or here, depends)
@@ -75,8 +92,8 @@ m64p_error VCR_StartMovie(char* path)
 		DebugMessage(M64MSG_INFO, "LoadM64 error: %s", strerror(errno));
 		return M64ERR_FILES;
 	}
-	SMovieHeader movieHeader; //@TODO move to heap if header will be needed in other places
-	fread(&movieHeader,sizeof(SMovieHeader),1,m64);
+	gMovieHeader = new SMovieHeader; //@TODO move to heap if header will be needed in other places
+	fread(gMovieHeader,sizeof(SMovieHeader),1,m64);
 
 	//this should never happen under normal conditions, means code logic error
 	if (gMovieBuffer != NULL)
@@ -86,7 +103,7 @@ m64p_error VCR_StartMovie(char* path)
 	}
 
 	//create a vector with enough space
-	gMovieBuffer = new std::vector<BUTTONS>(movieHeader.length_samples * movieHeader.num_controllers);
+	gMovieBuffer = new std::vector<BUTTONS>(gMovieHeader->length_samples * gMovieHeader->num_controllers);
 	if (m64 == NULL)
 	{
 		fclose(m64);
@@ -95,15 +112,15 @@ m64p_error VCR_StartMovie(char* path)
 	fread(gMovieBuffer->data(), sizeof(BUTTONS), gMovieBuffer->size(),m64);
 	fclose(m64);
 
-	if (movieHeader.startFlags == MOVIE_START_FROM_NOTHING)
+	if (gMovieHeader->startFlags == MOVIE_START_FROM_NOTHING)
 		;//restart and clear save data
-	else if (movieHeader.startFlags == MOVIE_START_FROM_EEPROM)
+	else if (gMovieHeader->startFlags == MOVIE_START_FROM_EEPROM)
 		;//restart
-	else if (movieHeader.startFlags == MOVIE_START_FROM_SNAPSHOT)
+	else if (gMovieHeader->startFlags == MOVIE_START_FROM_SNAPSHOT)
 		;//load .st
 
 	VCR_state = VCR_ACTIVE;
-	curFrame = 0;
+	curSample = 0;
 	VCR_SetReadOnly(true);
 	return M64ERR_SUCCESS;
 }
