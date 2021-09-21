81b66a908ac2949a3a6ea0715d78f9568fab6a86
Madghostek <madghostek@gmail.com>
2021-09-21 23:08:32 +0200

    Add resetting movie with stopMovie(bool)
    right now the tas.m64 loops itself at end for demonstration, rad

--- src/VCR/VCR.cpp
+++ src/VCR/VCR.cpp
@@ -17,21 +17,66 @@ extern "C" {
 #include "main/main.h"
 }
 
+//either holds current movie's path, or last movie. This could be initialised from config and have "load last m64" option?
+static char moviePath[PATH_MAX] = { 0 };
+
 static std::vector<BUTTONS> *gMovieBuffer = NULL; //holds reference to m64 data if movie is being played, otherwise NULL
 static SMovieHeader* gMovieHeader = NULL;
-static VCRState VCR_state = VCRState::VCR_IDLE;
+static VCRState VCR_state = VCR_IDLE;
 static bool VCR_readonly;
 static unsigned curSample = 0; //doesnt account in for multiple controllers, used as a pointer for vector
 
+
+
 #define BUFFER_GROWTH 256
 
-void VCR_StopMovie()
+/// <summary>
+/// Looks at m64 header flags and does appropriate things to core before starting movie.
+/// Its used in both start and restart so its hande to have it as a helper function.
+/// </summary>
+static void PrepareCore(char* path)
 {
-	VCR_state = VCR_IDLE;
-	delete gMovieBuffer;
-	delete gMovieHeader;
-	gMovieBuffer = NULL;
-	gMovieHeader  = NULL;
+	if (gMovieHeader->startFlags == MOVIE_START_FROM_NOTHING)
+		main_reset(true);
+	else if (gMovieHeader->startFlags == MOVIE_START_FROM_EEPROM)
+		//hard reset
+		//It doesn't detect if there were any polls, so for example using this with commandline will start emu
+		//and then instantly reset anyway
+		//@TODO: clear/ignore save data!!
+		main_reset(true);
+	else if (gMovieHeader->startFlags == MOVIE_START_FROM_SNAPSHOT)
+	{
+		char statePath[PATH_MAX];
+		strcpy(statePath, path);
+		strcpy(statePath + strlen(statePath) - 4, ".st");
+		main_state_load(statePath); //@TODO check file errors
+	}
+}
+
+void VCR_StopMovie(BOOL restart)
+{
+	if (!restart) //stop it
+	{
+		VCR_state = VCR_IDLE;
+		delete gMovieBuffer;
+		delete gMovieHeader;
+		gMovieBuffer = NULL;
+		gMovieHeader = NULL;
+	}
+	//if path exists
+	else if(moviePath[0]!='\0')
+	{
+		if (VCR_IsPlaying()) //no need to restart everything, just roll back
+		{
+			PrepareCore(moviePath);
+			curSample = 0;
+			VCR_SetReadOnly(true);
+		}
+		else //nothing is playing, load up saved path
+		{
+			VCR_StartMovie(moviePath);
+		}
+	}
 }
 
 
@@ -56,7 +101,7 @@ BOOL VCR_GetKeys(BUTTONS* keys, unsigned channel)
 		if (curSample >= gMovieBuffer->size())
 		{
 			//@TODO somehow notify frontend about that
-			VCR_StopMovie();
+			VCR_StopMovie(true);
 			return true; //ended
 		}
 	}
@@ -113,18 +158,10 @@ m64p_error VCR_StartMovie(char* path)
 	fread(gMovieBuffer->data(), sizeof(BUTTONS), gMovieBuffer->size(),m64);
 	fclose(m64);
 
-	if (gMovieHeader->startFlags == MOVIE_START_FROM_NOTHING)
-		;//restart and clear save data
-	else if (gMovieHeader->startFlags == MOVIE_START_FROM_EEPROM)
-		;//restart
-	else if (gMovieHeader->startFlags == MOVIE_START_FROM_SNAPSHOT)
-	{
-		char statePath[PATH_MAX];
-		strcpy(statePath, path);
-		strcpy(statePath + strlen(statePath) - 4, ".st");
-		main_state_load(statePath); //@TODO check file errors
-	}
+	PrepareCore(path);
 
+	//remember it
+	strcpy(moviePath,path);
 	VCR_state = VCR_ACTIVE;
 	curSample = 0;
 	VCR_SetReadOnly(true);
