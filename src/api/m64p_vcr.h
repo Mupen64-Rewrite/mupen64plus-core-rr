5115d07864c78098620437333f2b7fa946c56552
jgcodes2020 <jackyguo18@hotmail.com>
2022-05-18 13:38:45 -0400

    Fix random merge thing

--- src/api/m64p_vcr.h
+++ src/api/m64p_vcr.h
@@ -141,8 +141,4 @@ typedef m64p_error (*ptr_VCR_StartMovie)(char* path);
 }
 #endif
 
-<<<<<<< HEAD
 #endif
-=======
-#endif  /* #define M64P_VCR_H */
->>>>>>> 1c4552e8 (add API header for VCR pointers)
