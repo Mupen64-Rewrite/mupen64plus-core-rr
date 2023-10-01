8bb99b141ffa6a998b3b02025b87b12896a35852
jgcodes2020 <jackyguo18@hotmail.com>
2023-10-01 09:44:56 -0400

    RDRAM: improve error codes

--- src/api/rdram_api.c
+++ src/api/rdram_api.c
@@ -9,22 +9,22 @@ m64p_error RDRAM_ReadAligned(uint32_t addr, uint32_t* value) {
     if (value == NULL)
         return M64ERR_INPUT_ASSERT;
     if ((addr & 0x3) != 0)
-        return M64ERR_INPUT_INVALID;
+        return M64ERR_INPUT_ASSERT;
     
     struct device* const dev = &g_dev;
     
     if (!r4300_read_aligned_word(&dev->r4300, addr, value))
-        return M64ERR_INTERNAL;
+        return M64ERR_INPUT_NOT_FOUND;
     return M64ERR_SUCCESS;
 }
 m64p_error RDRAM_WriteAligned(uint32_t addr, uint32_t value, uint32_t mask) {
     if ((addr & 0x3) != 0)
-        return M64ERR_INPUT_INVALID;
+        return M64ERR_INPUT_ASSERT;
     
     
     struct device* const dev = &g_dev;
     
     if (!r4300_write_aligned_word(&dev->r4300, addr, value, mask))
-        return M64ERR_INTERNAL;
+        return M64ERR_INPUT_NOT_FOUND;
     return M64ERR_SUCCESS;
 }
