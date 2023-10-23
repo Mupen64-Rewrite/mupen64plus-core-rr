1fe5b384c49b41693b620d6a94e5d2d4acbc910c
jgcodes2020 <jackyguo18@hotmail.com>
2023-10-23 13:43:47 -0400

    fuse off backend encoder entirely

--- src/main/encoder.h
+++ src/main/encoder.h
@@ -10,10 +10,6 @@ extern "C" {
 extern void encoder_startup();
 extern void encoder_shutdown();
 
-extern m64p_error encoder_push_video();
-extern m64p_error encoder_push_audio(const void* data, size_t len);
-extern m64p_error encoder_set_sample_rate(unsigned int rate);
-
 // Audio stuff
 extern SampleCallback* g_sample_callback;
 extern RateChangedCallback* g_rate_changed_callback;
